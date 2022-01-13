833369d4cf47178d9f3a578bf531cd9db8e39dc8
Maxim Kvasnikov <MKvasnikov@integrity-solutions.ru>
2022-01-13 13:12:51 +0300

    feature property validation

--- src/main/java/ru/mxk/game/fifteen/configuration/GameOptions.java
+++ src/main/java/ru/mxk/game/fifteen/configuration/GameOptions.java
@@ -4,11 +4,20 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 
 @Getter
 @Setter
 @NoArgsConstructor
+@Validated
 @ConfigurationProperties(prefix = "game")
 public class GameOptions {
-    private int size;
+    @Min(1)
+    @Max(30)
+    @NotNull
+    private Integer size;
 }
